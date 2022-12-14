016e0c038fd7e5304b9f52f8986f13e6f59f1718
DNNguyen-2003 <dnnguyen3152003@gmail.com>
2022-12-14 10:39:36 +0700

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { links } from "./data/links";
 import "./Header.css";
 import logo from "./img/logo_white.svg";
+import logo1 from "./img/logo_black.svg";
 import "../sideBar/SideBar";
 import SideBar from "../sideBar/SideBar";
 export default function Header() {
+  const navRef = useRef();
+  const logoRef = useRef();
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        navRef.current.classList.add("navActive");
+        logoRef.current.src = logo1;
+      } else {
+        navRef.current.classList.remove("navActive");
+        logoRef.current.src = logo;
+      }
+    });
+  });
   const [sideBar, setSideBar] = useState(false);
   const toggleSideBar = () => {
     setSideBar(!sideBar);
   };
   return (
-    <header className="header">
+    <header className="header" ref={navRef}>
       <div className="navBar">
         <div className="logo">
-          <img src={logo} alt="" />
+          <img ref={logoRef} src={logo} alt="" />
         </div>
         <div className="links">
           {links.map((link) => {
