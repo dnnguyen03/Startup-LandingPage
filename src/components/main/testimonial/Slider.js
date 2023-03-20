b9198ca1c580b0c5907137521bba08845b335e4a
DNNguyen-2003 <dnnguyen3152003@gmail.com>
2023-03-20 15:14:23 +0700

    update

--- src/components/main/testimonial/Slider.js
+++ src/components/main/testimonial/Slider.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import { dataSlider } from "../../../utils/data";
 import { ReactComponent as Star } from "../../../asset/image/star.svg";
+import { ReactComponent as Next } from "../../../asset/image/next.svg";
+import { ReactComponent as Prev } from "../../../asset/image/prev.svg";
 function Slide(props) {
   return (
     <div className="slide">
@@ -83,7 +85,7 @@ export default function Slider() {
       </div>
       <div className="container">
         <div className="slider" ref={listSlide}>
-          {dataSlider.map((slide, index) => {
+          {dataSlider.map((slide) => {
             return (
               <Slide
                 key={slide.id}
@@ -99,29 +101,11 @@ export default function Slider() {
         </div>
         <div className="control_slide">
           <button className="prev" onClick={prev}>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M216.4 163.7c5.1 5 5.1 13.3.1 18.4L155.8 243h231.3c7.1 0 12.9 5.8 12.9 13s-5.8 13-12.9 13H155.8l60.8 60.9c5 5.1 4.9 13.3-.1 18.4-5.1 5-13.2 5-18.3-.1l-82.4-83c-1.1-1.2-2-2.5-2.7-4.1-.7-1.6-1-3.3-1-5 0-3.4 1.3-6.6 3.7-9.1l82.4-83c4.9-5.2 13.1-5.3 18.2-.3z"></path>
-            </svg>
+            <Prev></Prev>
           </button>
 
           <button className="next" onClick={next}>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M295.6 163.7c-5.1 5-5.1 13.3-.1 18.4l60.8 60.9H124.9c-7.1 0-12.9 5.8-12.9 13s5.8 13 12.9 13h231.3l-60.8 60.9c-5 5.1-4.9 13.3.1 18.4 5.1 5 13.2 5 18.3-.1l82.4-83c1.1-1.2 2-2.5 2.7-4.1.7-1.6 1-3.3 1-5 0-3.4-1.3-6.6-3.7-9.1l-82.4-83c-4.9-5.2-13.1-5.3-18.2-.3z"></path>
-            </svg>
+            <Next></Next>
           </button>
         </div>
       </div>
